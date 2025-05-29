d6fdc6336360e38c0ca4cff1b80e0fd6e728189d
Kebhino <wrobeljakub@gmail.com>
2025-05-29 11:16:48 +0200

    Handling errors

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -3,22 +3,32 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Todo } from "./hooks/useTodos";
 import axios from "axios";
 
+interface AddTodoContex {
+  previousTodos: Todo[];
+}
+
 const TodoForm = () => {
   const queryClient = useQueryClient();
-  const addTodo = useMutation<Todo, Error, Todo>({
+  const addTodo = useMutation<Todo, Error, Todo, AddTodoContex>({
     mutationFn: (todo: Todo) =>
       axios
         .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
         .then((res) => res.data),
 
     onMutate: (newTodo: Todo) => {
+      const previousTodos = queryClient.getQueryData<Todo[]>(["todos"]) || [];
       queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
         newTodo,
         ...(todos || []),
       ]);
       if (ref.current) ref.current.value = "";
+      return { previousTodos };
     },
     onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) =>
+        todos?.map((todo) => (todo === newTodo ? savedTodo : todo))
+      );
+
       // 1 SPOSÓB:
       // I teraz trzeba powiedzieć do ReactQuery że jest coś nowego i dane ma stare, żeby pobrał sobie nowe.
       //     queryClient.invalidateQueries({
@@ -27,6 +37,11 @@ const TodoForm = () => {
       //   },
       // });
       // 2 SPOSÓB Aktulizujemy dane w cache bezpośrednio
+    },
+    onError: (error, newTodo, contex) => {
+      if (!contex) return;
+
+      queryClient.setQueryData<Todo[]>(["todos"], contex.previousTodos);
     },
   });
 
