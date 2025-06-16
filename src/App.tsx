1a88722814bbde8a8aa18e05462a316cc849b097
Kebhino <wrobeljakub@gmail.com>
2025-06-16 10:31:23 +0200

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useReducer } from "react";
 import "./App.css";
 import taskReducer from "./state-management/reducers/taskReducer";
-
 import TaskContext from "./state-management/contexts/tasksContext";
 import NavBar from "./state-management/NavBar";
 import HomePage from "./state-management/HomePage";
