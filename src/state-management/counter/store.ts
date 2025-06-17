e817536cf204520845a8941dbb252d0d30715061
Kebhino <wrobeljakub@gmail.com>
2025-06-17 11:58:46 +0200

    DevTools

--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 interface CounterStore { 
     counter: number;
@@ -16,4 +17,6 @@ reset: () => set(() => ({max: 10}))
 
 }))
 
+if (process.env.NODE_ENV === "development")
+    mountStoreDevtool('Counter Store', useCounterStore)
 export default useCounterStore
